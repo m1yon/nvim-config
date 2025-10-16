6dd65ea3a256f51d2a28c8f373d4a26f787ac11b
Michael Lyon <mlyon360@gmail.com>
2025-10-16 17:15:16 -0600

    add linter

--- plugin/40_plugins.lua
+++ plugin/40_plugins.lua
@@ -112,6 +112,30 @@ now_if_args(function()
 	})
 end)
 
+-- Linting ====================================================================
+later(function()
+	add("mfussenegger/nvim-lint")
+	local lint = require("lint")
+	lint.linters_by_ft = {
+		go = { "golangcilint" },
+	}
+
+	-- Create autocommand which carries out the actual linting
+	-- on the specified events.
+	local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+	vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+		group = lint_augroup,
+		callback = function()
+			-- Only run the linter in buffers that you can modify in order to
+			-- avoid superfluous noise, notably within the handy LSP pop-ups that
+			-- describe the hovered symbol using Markdown.
+			if vim.bo.modifiable then
+				lint.try_lint()
+			end
+		end,
+	})
+end)
+
 -- Formatting =================================================================
 
 -- Programs dedicated to text formatting (a.k.a. formatters) are very useful.
