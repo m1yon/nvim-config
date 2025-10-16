d2d3ee6fcc358d41d2db81808eb310986893ac95
Michael Lyon <mlyon360@gmail.com>
2025-10-16 17:13:20 -0600

    add formatters

--- plugin/40_plugins.lua
+++ plugin/40_plugins.lua
@@ -29,45 +29,53 @@ local now_if_args = _G.Config.now_if_args
 --
 -- Add these plugins now if file (and not 'mini.starter') is shown after startup.
 now_if_args(function()
-  add({
-    source = 'nvim-treesitter/nvim-treesitter',
-    -- Use `main` branch since `master` branch is frozen, yet still default
-    checkout = 'main',
-    -- Update tree-sitter parser after plugin is updated
-    hooks = { post_checkout = function() vim.cmd('TSUpdate') end },
-  })
-  add({
-    source = 'nvim-treesitter/nvim-treesitter-textobjects',
-    -- Same logic as for 'nvim-treesitter'
-    checkout = 'main',
-  })
+	add({
+		source = "nvim-treesitter/nvim-treesitter",
+		-- Use `main` branch since `master` branch is frozen, yet still default
+		checkout = "main",
+		-- Update tree-sitter parser after plugin is updated
+		hooks = {
+			post_checkout = function()
+				vim.cmd("TSUpdate")
+			end,
+		},
+	})
+	add({
+		source = "nvim-treesitter/nvim-treesitter-textobjects",
+		-- Same logic as for 'nvim-treesitter'
+		checkout = "main",
+	})
 
-  -- Ensure installed parsers for listed languages. Add to `languages`
-  -- array languages which you want to have installed. To see available languages:
-  -- - Execute `:=require('nvim-treesitter').get_available()`
-  -- - Visit
-  --   https://github.com/nvim-treesitter/nvim-treesitter/blob/main/SUPPORTED_LANGUAGES.md
-  local ensure_languages = {
-    -- These are already installed. Used as an example.
-    'lua',
-    'vimdoc',
-    'markdown',
-  }
-  local isnt_installed = function(lang)
-    return #vim.api.nvim_get_runtime_file('parser/' .. lang .. '.*', false) == 0
-  end
-  local to_install = vim.tbl_filter(isnt_installed, ensure_languages)
-  if #to_install > 0 then require('nvim-treesitter').install(to_install) end
+	-- Ensure installed parsers for listed languages. Add to `languages`
+	-- array languages which you want to have installed. To see available languages:
+	-- - Execute `:=require('nvim-treesitter').get_available()`
+	-- - Visit
+	--   https://github.com/nvim-treesitter/nvim-treesitter/blob/main/SUPPORTED_LANGUAGES.md
+	local ensure_languages = {
+		-- These are already installed. Used as an example.
+		"lua",
+		"vimdoc",
+		"markdown",
+	}
+	local isnt_installed = function(lang)
+		return #vim.api.nvim_get_runtime_file("parser/" .. lang .. ".*", false) == 0
+	end
+	local to_install = vim.tbl_filter(isnt_installed, ensure_languages)
+	if #to_install > 0 then
+		require("nvim-treesitter").install(to_install)
+	end
 
-  -- Ensure tree-sitter enabled after opening a file for target language
-  local filetypes = {}
-  for _, lang in ipairs(ensure_languages) do
-    for _, ft in ipairs(vim.treesitter.language.get_filetypes(lang)) do
-      table.insert(filetypes, ft)
-    end
-  end
-  local ts_start = function(ev) vim.treesitter.start(ev.buf) end
-  _G.Config.new_autocmd('FileType', filetypes, ts_start, 'Start tree-sitter')
+	-- Ensure tree-sitter enabled after opening a file for target language
+	local filetypes = {}
+	for _, lang in ipairs(ensure_languages) do
+		for _, ft in ipairs(vim.treesitter.language.get_filetypes(lang)) do
+			table.insert(filetypes, ft)
+		end
+	end
+	local ts_start = function(ev)
+		vim.treesitter.start(ev.buf)
+	end
+	_G.Config.new_autocmd("FileType", filetypes, ts_start, "Start tree-sitter")
 end)
 
 -- Language servers ===========================================================
@@ -86,14 +94,14 @@ end)
 --
 -- Add it now if file (and not 'mini.starter') is shown after startup.
 now_if_args(function()
-  add('neovim/nvim-lspconfig')
+	add("neovim/nvim-lspconfig")
 
-  -- Use `:h vim.lsp.enable()` to automatically enable language server based on
-  -- the rules provided by 'nvim-lspconfig'.
-  -- Use `:h vim.lsp.config()` or 'ftplugin/lsp/' directory to configure servers.
-  -- Uncomment and tweak the following `vim.lsp.enable()` call to enable servers.
-  vim.lsp.enable({
-	  "gopls",
+	-- Use `:h vim.lsp.enable()` to automatically enable language server based on
+	-- the rules provided by 'nvim-lspconfig'.
+	-- Use `:h vim.lsp.config()` or 'ftplugin/lsp/' directory to configure servers.
+	-- Uncomment and tweak the following `vim.lsp.enable()` call to enable servers.
+	vim.lsp.enable({
+		"gopls",
 		"lua_ls",
 		"pyright",
 		"ts_ls",
@@ -101,7 +109,7 @@ now_if_args(function()
 		"yamlls",
 		"tailwindcss",
 		"eslint",
-  })
+	})
 end)
 
 -- Formatting =================================================================
@@ -113,17 +121,33 @@ end)
 -- The 'stevearc/conform.nvim' plugin is a good and maintained solution for easier
 -- formatting setup.
 later(function()
-  add('stevearc/conform.nvim')
+	add("stevearc/conform.nvim")
 
-  -- See also:
-  -- - `:h Conform`
-  -- - `:h conform-options`
-  -- - `:h conform-formatters`
-  require('conform').setup({
-    -- Map of filetype to formatters
-    -- Make sure that necessary CLI tool is available
-    -- formatters_by_ft = { lua = { 'stylua' } },
-  })
+	-- See also:
+	-- - `:h Conform`
+	-- - `:h conform-options`
+	-- - `:h conform-formatters`
+	require("conform").setup({
+		-- Map of filetype to formatters
+		formatters_by_ft = {
+			go = { "gofumpt", "goimports", "golines" },
+			typescriptreact = { "prettierd" },
+			typescript = { "prettierd" },
+			javascript = { "prettierd" },
+			json = { "prettierd" },
+			lua = { "stylua" },
+			python = { "ruff_fix", "ruff_format" },
+			yaml = { "prettierd" },
+		},
+
+		formatters = {},
+
+		format_on_save = {
+			-- These options will be passed to conform.format()
+			timeout_ms = 500,
+			lsp_format = "fallback",
+		},
+	})
 end)
 
 -- Snippets ===================================================================
@@ -135,7 +159,9 @@ end)
 -- snippet files. They are organized in 'snippets/' directory (mostly) per language.
 -- 'mini.snippets' is designed to work with it as seamlessly as possible.
 -- See `:h MiniSnippets.gen_loader.from_lang()`.
-later(function() add('rafamadriz/friendly-snippets') end)
+later(function()
+	add("rafamadriz/friendly-snippets")
+end)
 
 -- Honorable mentions =========================================================
 
@@ -148,8 +174,8 @@ later(function() add('rafamadriz/friendly-snippets') end)
 --
 -- You can use it like so:
 later(function()
-  add('mason-org/mason.nvim')
-  require('mason').setup()
+	add("mason-org/mason.nvim")
+	require("mason").setup()
 end)
 
 -- Beautiful, usable, well maintained color schemes outside of 'mini.nvim' and
