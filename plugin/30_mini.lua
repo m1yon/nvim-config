625426089875358986379d8b9c3aca06bcda9037
Michael Lyon <mlyon360@gmail.com>
2025-10-17 10:21:21 -0600

    Revert "remove mini starter"
    This reverts commit 3b0ca8f39f131a72b3073e1f706f211d16faf543.

--- plugin/30_mini.lua
+++ plugin/30_mini.lua
@@ -136,6 +136,17 @@ now(function()
 	require("mini.sessions").setup()
 end)
 
+-- Start screen. This is what is shown when you open Neovim like `nvim`.
+-- Example usage:
+-- - Type prefix keys to limit available candidates
+-- - Navigate down/up with `<C-n>` and `<C-p>`
+-- - Press `<CR>` to select an entry
+--
+-- See also:
+-- - `:h MiniStarter-example-config` - non-default config examples
+-- - `:h MiniStarter-lifecycle` - how to work with Starter buffer
+now(function() require('mini.starter').setup() end)
+
 -- Statusline. Sets `:h 'statusline'` to show more info in a line below window.
 -- Example usage:
 -- - Left most section indicates current mode (text + highlighting).
