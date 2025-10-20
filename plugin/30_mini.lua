a1bf007caac4a5826262d2bc3c8d8d86be71f8bc
Michael Lyon <mlyon360@gmail.com>
2025-10-20 13:12:45 -0600

    switch to dracula theme

--- plugin/30_mini.lua
+++ plugin/30_mini.lua
@@ -35,7 +35,7 @@ local now_if_args = _G.Config.now_if_args
 -- - `:h MiniHues-examples` - how to define highlighting with 'mini.hues'
 -- - 'plugin/40_plugins.lua' honorable mentions - other good color schemes
 now(function()
-	require("mini.hues").setup({ background = "#050817", foreground = "#cbd5e1", accent = "green" })
+	require("mini.hues").setup({ background = "#282a36", foreground = "#f8f8f2" })
 end)
 
 -- You can try these other 'mini.hues'-based color schemes (uncomment with `gcc`):
